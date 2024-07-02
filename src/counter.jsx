89384e844d32d8ca8060511a1c1ed13467c4f59a
tsegaye abadi <tsegaye.abadi15@gmail.com>
2024-07-01 22:02:03 -0400

    counter added

---
+++ src/counter.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
+
+
